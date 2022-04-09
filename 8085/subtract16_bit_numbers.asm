113e17224c6142a09eded04e2e3db50386bbbd64
roopaish <54392112+Roopaish@users.noreply.github.com>
2022-04-09 08:43:38 +0545

    correction

--- 8085/subtract16_bit_numbers.asm
+++ 8085/subtract16_bit_numbers.asm
@@ -2,9 +2,13 @@ LHLD 2000H  ; H = [2000], L = [2001]
 XCHG        ; D <-> H, E <-> L
 LHLD 2002H  ; H = [2002], L = [2003]
 MOV A, E    ; A = E 
+
 SUB L       ; A = A - L = E - L
-STA 2017H   ; [2017] = A
+MOV L, A    ; L = A, Store sum of lower byte
+
 MOV A, D    ; A = D
 SBB H       ; A = A - H with Borrow = D - H
-STA 2018H   ; [2018] = A
+MOV H, A    ; H = A, Store sum of highest byte
+
+SHLD 2017h  ; [2017] = H, [2018] = L
 HLT
---
+++ extract_certain_words_from_string.asm
@@ -0,0 +1,39 @@
+; Given: Microprocessor and Assembly Language Programming
+; Display: Microprocessor Programming
+
+.model small
+.stack
+.data
+    str dB "Microprocessor and Assembly Language Programming$";
+.code
+    main proc
+        .startup
+        ; Microprocessor
+        mov cx, 14h
+        mov si, offset str
+        
+        label1:             ; Looping through each character
+        mov dl, [si]
+        mov ah, 02h
+        int 21h
+        inc si
+        loop label1         ; loops until cx becomes 0
+        
+        
+        ; Programming
+        mov cx, 11h
+        mov si, offset str
+        
+        add [si], 36h
+        
+        label2:
+        mov dl, [si]
+        mov ah, 02h
+        int 21h
+        inc si
+        loop label2
+        
+        .exit
+    main endp
+end main
+        
