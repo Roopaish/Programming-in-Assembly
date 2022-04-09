482fca52b4d1ef495ca31833b1de2518877a88a0
roopaish <54392112+Roopaish@users.noreply.github.com>
2022-04-09 08:24:29 +0545

    add inputted numbers

---
+++ add_input_numbers.asm
@@ -0,0 +1,32 @@
+.model small
+.stack
+.data
+.code
+    main proc
+        .startup
+        mov ah, 01h
+        int 21h
+        sub al, 30h   ; ascii -> hex
+        
+        mov bl, al    ; bl = 1st number
+        
+        mov ah, 01
+        int 21h
+        sub al, 30h   ; a = 2nd number, ascii -> hex
+        
+        add al, bl
+        jnc skip
+        mov dl, 01h
+        mov ah, 02h  ; display carry
+        int 21h
+        
+        skip:
+        add al, 30h  ; hex -> ascii
+        
+        mov dl, al
+        mov ah, 02h  ; display sum
+        int 21h
+        
+        .exit
+    main endp
+end main
